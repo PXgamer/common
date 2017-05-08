58aae13165a35a8653a99dd753416d67af097970
PXgamer <owzie123@gmail.com>
2017-05-08 10:57:40 +0100

    Added Scalars\Arrays class

---
+++ src/Scalars/Arrays.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace pxgamer\Common\Scalars;
+
+/**
+ * Class Arrays
+ * @package pxgamer\Common\Scalars
+ */
+class Arrays
+{
+    /**
+     * @param array $array
+     * @param string $delimiter
+     * @param bool $base_val
+     * @return array|bool
+     */
+    public static function to_tree($array, $delimiter = '_', $base_val = false)
+    {
+        if (!is_array($array)) {
+            return false;
+        }
+        $splitRE = '/' . preg_quote($delimiter, '/') . '/';
+        $returnArr = array();
+        foreach ($array as $key => $val) {
+            // Get parent parts and the current leaf
+            $parts = preg_split($splitRE, $key, -1, PREG_SPLIT_NO_EMPTY);
+            $leafPart = array_pop($parts);
+
+            // Build parent structure
+            // Might be slow for really deep and large structures
+            $parentArr = &$returnArr;
+            foreach ($parts as $part) {
+                if (!isset($parentArr[$part])) {
+                    $parentArr[$part] = array();
+                } elseif (!is_array($parentArr[$part])) {
+                    if ($base_val) {
+                        $parentArr[$part] = array('__base_val' => $parentArr[$part]);
+                    } else {
+                        $parentArr[$part] = array();
+                    }
+                }
+                $parentArr = &$parentArr[$part];
+            }
+
+            // Add the final part to the structure
+            if (empty($parentArr[$leafPart])) {
+                $parentArr[$leafPart] = $val;
+            } elseif ($base_val && is_array($parentArr[$leafPart])) {
+                $parentArr[$leafPart]['__base_val'] = $val;
+            }
+        }
+        return $returnArr;
+    }
+}
