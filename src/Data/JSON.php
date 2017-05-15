043f76e00f9b5eccc687aff7940a295f20d74367
PXgamer <owzie123@gmail.com>
2017-05-15 14:29:37 +0100

    Added PhpDoc comments to Data\JSON

--- src/Data/JSON.php
+++ src/Data/JSON.php
@@ -2,8 +2,31 @@
 
 namespace pxgamer\Common\Data;
 
+/**
+ * Class JSON
+ * @package pxgamer\Common\Data
+ */
 class JSON
 {
+    /**
+     * Allows you to do object additions such as below:
+     * ```
+     * $json = new JSON();
+     * $json->message = 'This is JSON.';
+     * $json->code = 123;
+     * echo $json;
+     * ```
+     *
+     * Which would output:
+     * ```
+     *  {
+     *    'message' => 'This is JSON.',
+     *    'code' => 123
+     *  }
+     * ```
+     *
+     * @return string
+     */
     public function __toString()
     {
         return json_encode($this);
