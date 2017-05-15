0616ddac3acebf838d8316222c542efc846a5a4f
PXgamer <owzie123@gmail.com>
2017-05-15 10:38:18 +0100

    Added default data for Errors\Error

--- src/Errors/Error.php
+++ src/Errors/Error.php
@@ -8,6 +8,13 @@ class Error
     protected $text;
     protected $data;
 
+    public function __construct()
+    {
+        $this->code = 0;
+        $this->text = 'An unknown error occurred.';
+        $this->data = null;
+    }
+
     public function __get($name)
     {
         if ($this->$name ?? false) {
